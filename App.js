2aef4fa17eee3ae1d8ec40dcacde6d7b45dde156
Abdul Moid Babar <abdulmoidbabar@Abduls-MacBook-Pro.local>
2021-07-01 11:51:05 +0100

    Task complete

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import Task from './src/Task'
+
+function App(props) {
+  return (
+    <View>
+      <Task/>
+    </View>
+  );
+}
+
+export default App;
